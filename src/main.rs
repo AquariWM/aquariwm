589604ae9ed49c2fc72c129450ae760b9ec04bc7
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-06-19 01:30:55 +0400

    Connect to X server & comment useful resources

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-fn main() {
-    println!("This is AquariWM!");
+// See:
+// ============================
+// rust-xcb docs - https://rust-x-bindings.github.io/rust-xcb/xcb/index.html
+// rust-xcb repo - https://github.com/rust-x-bindings/rust-xcb
+//
+// how X window managers work and how to write one:
+//      https://jichu4n.com/posts/how-x-window-managers-work-and-how-to-write-one-part-i/
+//
+// Inter-Client Communication Conventions Manual - https://tronche.com/gui/x/icccm/
+// Extended Window Manager Hints - https://specifications.freedesktop.org/wm-spec/latest/
+
+fn main() -> xcb::Result<()> {
+    let _conn = xcb::Connection::connect(None)?;
+
+    print!("Woohoo! The crowd goes wild! This should be an X connection that disconnects ");
+    print!("when <_conn> goes out of sync...");
+
+    Ok(())
 }
