ceaddc896aa7b306b780dd003f7bca1cea76475f
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-06-21 15:43:59 +0400

    Starter test features comment

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@
 // Inter-Client Communication Conventions Manual - https://tronche.com/gui/x/icccm/
 // Extended Window Manager Hints - https://specifications.freedesktop.org/wm-spec/latest/
 
-fn main() -> xcb::Result<()> {
-    let _conn = xcb::Connection::connect(None)?;
-
-    print!("Woohoo! The crowd goes wild! This should be an X connection that disconnects ");
-    print!("when <_conn> goes out of sync...");
-
-    Ok(())
-}
+// The following code and features are in no way representative of what AquariWM is designed to be,
+// nor what it will be for much longer. It is simply a test, to see what the implementation of
+// basic window manager functions will involve...
+//
+// If you are reading this message, and you would like to contribute to the window manager: do feel
+// free to remove all of the contents of this file and start over (but make sure to include the
+// license header for the MPL-2.0). The first tasks for the real window manager design are to get
+// the fundamentals of module communication set up: try to have a custom message sent through an
+// IPC socket to AquariWM with information on where to put the focused window, and then have the
+// AquariWM core move the focused window based on those coordinates received over IPC.
