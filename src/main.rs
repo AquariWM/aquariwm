0412a872e70fb51ab1e56158c9957dd0d1a93b77
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-07-07 17:25:31 +0400

    Move code plan comments inside main()

--- src/main.rs
+++ src/main.rs
@@ -34,40 +34,38 @@ fn main() -> xcb::Result<()> {
 	// connect to the X server
 	let (conn, screen) = xcb::Connection::connect(None)?;
 
-	// register for SUBSTRUCTURE_NOTIFY and SUBSTRUCTURE_REDIRECT on the root window...
+	// Window managers in X are simply clients that have permission to perform substructure redirection
+	// on the root window. Only one such client can be active at once. Start by informing X that we
+	// would like to select input on the root window for the substrcuture redirect mask and the
+	// substructure notify mask.
+	//
+	// Potentially helpful example code (Rust, XCB):
+	// https://github.com/mjkillough/lanta/blob/4c31f087514502f243eb15ac0f1a57072aa8779c/src/x.rs#L173
+	//
+	// xcb::EVENT_MASK_SUBSTRUCTURE_NOTIFY
+	// xcb::EVENT_MASK_SUBSTRUCTURE_REDIRECT
+	// (substructure redirect on root window with xcb::change_window_attributes_checked)
+
+	// Start by telling X what we want to receive events for. We want to receive input events for
+	// cursor movement (motion events), Super + Left Mouse Button, Super + Right Mouse Button, and
+	// Super + f.
+
+	// select mouse motion input
+	// grab Super + Left Mouse Button
+	// grab Super + Right Mouse Button
+	// grab Super + f
+
+	// event loop {
+	// //
+	// // mouse motion {
+	// // // if (hovered window != focused window) focus(hovered window)
+	// // }
+	// //
+	// // Super + Left Mouse Button -> move window
+	// // Super + Right Mouse Button -> resize window
+	// // Super + F -> toggle fullscreen on the window
+	// //
+	// }
 
 	Ok(())
 }
-
-// Window managers in X are simply clients that have permission to perform substructure redirection
-// on the root window. Only one such client can be active at once. Start by informing X that we
-// would like to select input on the root window for the substrcuture redirect mask and the
-// substructure notify mask.
-//
-// Potentially helpful example code (Rust, XCB):
-// https://github.com/mjkillough/lanta/blob/4c31f087514502f243eb15ac0f1a57072aa8779c/src/x.rs#L173
-//
-// xcb::EVENT_MASK_SUBSTRUCTURE_NOTIFY
-// xcb::EVENT_MASK_SUBSTRUCTURE_REDIRECT
-// (substructure redirect on root window with xcb::change_window_attributes_checked)
-
-// Start by telling X what we want to receive events for. We want to receive input events for
-// cursor movement (motion events), Super + Left Mouse Button, Super + Right Mouse Button, and
-// Super + f.
-
-// select mouse motion input
-// grab Super + Left Mouse Button
-// grab Super + Right Mouse Button
-// grab Super + f
-
-// event loop {
-// //
-// // mouse motion {
-// // // if (hovered window != focused window) focus(hovered window)
-// // }
-// //
-// // Super + Left Mouse Button -> move window
-// // Super + Right Mouse Button -> resize window
-// // Super + F -> toggle fullscreen on the window
-// //
-// }
