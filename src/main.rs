812affa03be21360bd3ea6e3c2d0b84b4cab5505
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-07-19 08:03:19 +1200

    Move event handling to separate functions

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@
 use xcb::x;
 
 /// A primitive base window manager implementation for AquariWM to build upon.
+///
 /// This is not the proper implementation of AquariWM and its module system, but rather a very
 /// basic floating window manager that can be built upon in time. It supports the basic
 /// functions of moving windows, resizing windows, focusing a particular window, and toggling
@@ -34,8 +35,7 @@ fn main() -> xcb::Result<()> {
 		)],
 	});
 
-	// As the substructure redirection request is not checked, we must flush the connection to
-	// make the X server aware of our request.
+	// Flush the queued request to the X server.
 	conn.flush()?;
 
 	// Send a request asking to receive events relating to the cursor motion.
@@ -59,81 +59,91 @@ fn main() -> xcb::Result<()> {
 	// We wait for all the replies to be received at once, so that there is no need to be waiting
 	// when we can be sending the other requests. As there is no reply from substructure
 	// redirection, there is only one such reply for the moment.
+	// TODO: do we have to wait for the reply? perhaps we can flush the connection just like when
+	//       we aren't expecting any reply? since we do nothing with the reply, it might be better
+	//       to flush if it is possible.
 	conn.wait_for_reply(cookie)?;
 
+	// Run the event loop and return its value (that's why the semicolon is missing).
 	run(conn)
 }
 
 /// The main event loop of the window manager, where it handles received events.
+///
 /// The event loop waits until the program receives a new event from the X server, and then, based
 /// on the event type received, it reacts accordingly (sending new requests to the X server when
 /// necessary).
 fn run(conn: xcb::Connection) -> xcb::Result<()> {
-	// This is the main event loop. In here, we wait until an event is sent to AquariWM by the X
-	// server, and then, based on the event received, we choose to react accordingly, or not at
-	// all.
 	loop {
 		// Receive the next event from the X server, when available, and match against its type.
 		match conn.wait_for_event()? {
 			// Honor window configure requests completely, for now.
 			xcb::Event::X(x::Event::ConfigureRequest(req)) => {
-				// As we have registered for substructure redirection on the root window, it is
-				// now our job to respond to requests made by other clients. The X server was
-				// doing a perfectly fine job for our current needs though, so we'll just send it
-				// right back, just as before.
-				conn.send_request(&x::ConfigureWindow {
-					window: req.window(),
-					value_list: &[
-						x::ConfigWindow::X(req.x().into()),
-						x::ConfigWindow::Y(req.y().into()),
-						x::ConfigWindow::Width(req.width().into()),
-						x::ConfigWindow::Height(req.height().into()),
-						x::ConfigWindow::BorderWidth(req.border_width().into()),
-						x::ConfigWindow::Sibling(req.sibling()),
-						x::ConfigWindow::StackMode(req.stack_mode()),
-					],
-				});
-
-				// Flush the connection manually, as there is no reply.
-				conn.flush()?;
+				on_configure(&conn, req)?;
 			}
 			// Map windows after creation.
 			xcb::Event::X(x::Event::MapRequest(req)) => {
-				// In the real window manager, this is where the decorator module would come in.
-				// The decorator module's job would be to populate a frame around the window with
-				// window decorations, such as a title bar, a close button, etc. We would first
-				// ask the decorator module if it even wants to decorate the window in particular,
-				// I mean, no point in creating a frame for a window that doesn't need one. If the
-				// decorator module wants to decorate the window, we can create a new frame window
-				// with the appropriate position and size given by the layout module, and then ask
-				// the decorator module to do its thing. The decorator module would send us a reply
-				// indicating the area left, free from decoration, to place the real window within.
-				// We would  register for substructure redirection on the frame window (as
-				// substructure redirection only applies to direct children, and this would make
-				// the real window a direct child of the frame window, instead of the root window),
-				// and then reparent the real window to this frame window. Finally, after all that,
-				// we could map the frame window and the real window on top.
-				//
-				// We're not actually doing any of that right now though. Now we're just mapping
-				// the 'real window' directly with no window decorations.
-
-				conn.send_request(&x::MapWindow {
-					window: req.window(),
-				});
-
-				// Flush the connection.
-				conn.flush()?;
-			}
-			// Print the cursor position.
-			xcb::Event::X(x::Event::MotionNotify(motion)) => {
-				println!(
-					"The cursor position is {}, {}.",
-					motion.root_x(),
-					motion.root_y()
-				);
+				on_map(&conn, req)?;
 			}
 			// Ignore any other events.
 			_ => {}
 		}
 	}
+}
+
+/// Handle a client's request to configure a window.
+///
+/// When the window manager receives a request to configure a window, this function is called.
+/// Currently, we simply send the exact same request back to the X server with no changes, but we
+/// may wish to modify this request in the future. X clients must accept any modification we make
+/// to their requests.
+fn on_configure(conn: &xcb::Connection, req: x::ConfigureRequestEvent) -> xcb::Result<()> {
+	// We simply send an identical request back to the X server. The numerical units received here
+	// must be converted with `.into()` as the size of integer received in the `ConfigureRequest`
+	// is smaller than that which the X server expects.
+	conn.send_request(&x::ConfigureWindow {
+		window: req.window(),
+		value_list: &[
+			x::ConfigWindow::X(req.x().into()),
+			x::ConfigWindow::Y(req.y().into()),
+			x::ConfigWindow::Width(req.width().into()),
+			x::ConfigWindow::Height(req.height().into()),
+			x::ConfigWindow::BorderWidth(req.border_width().into()),
+			x::ConfigWindow::Sibling(req.sibling()),
+			x::ConfigWindow::StackMode(req.stack_mode()),
+		],
+	});
+
+	conn.flush()?;
+	Ok(())
+}
+
+/// Handle a client's request to map a window, reparenting if necessary.
+///
+/// Here we simply 'bounce back' a MapRequest to the X server, but in the future we can create a
+/// frame window here and reparent the client window to it so that window decorations can exist.
+fn on_map(conn: &xcb::Connection, req: x::MapRequestEvent) -> xcb::Result<()> {
+	// In the real window manager, this is where the decorator module would come in. The decorator
+	// module's job would be to populate a frame around the window with window decorations, such
+	// as a title bar, a close button, etc. We would first ask the decorator module if it even
+	// wants to decorate the window in particular, as there's no point in creating a frame for a
+	// window that doesn't need one. If the decorator module wants to decorate the window, we can
+	// create a new frame window with the appropriate position and size given by the layout
+	// module, and then ask the decorator module to do its thing. The decorator module would send
+	// us a reply indicating the area left, free from decoration, to place the real window within.
+	// We would register for substructure redirection on the frame window (as substructure
+	// redirection only applies to direct children, and this would make the real window a direct
+	// child of the frame window, instead of the root window), and then reparent the real window
+	// to this frame window. Finally, after all that, we could map the frame window and the real
+	// window on top.
+	//
+	// We're not actually doing any of that right now though. Now we're just mapping the 'real
+	// window' directly with no window decorations.
+
+	conn.send_request(&x::MapWindow {
+		window: req.window(),
+	});
+
+	conn.flush()?;
+	Ok(())
 }
