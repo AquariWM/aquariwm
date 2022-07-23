9ad92ea3b3a99a0d789947aea70417fba2ddbe8e
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-07-23 16:15:49 +1200

    Add license header to bindings.rs

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use xcb::x::{self, ConfigWindow, ConfigWindowMask, StackMode, Window};
 
 /// Allows wrapping an object of type `T` to instantiate [`Self`] and unwrapping to get it back.
