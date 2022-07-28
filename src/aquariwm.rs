ef5ee7f5297774063a7819c44633894cc42a0283
Antikyth <104020300+Antikyth@users.noreply.github.com>
2022-07-28 23:16:47 +1200

    Format: aquariwm module

--- src/aquariwm.rs
+++ src/aquariwm.rs
@@ -14,40 +14,50 @@ pub struct AquariWm {
 }
 
 impl AquariWm {
-    /// Instantiates the window manager object with the given [Connection] and root
-    /// [window](Window).
 	pub fn new(conn: Connection, root: Window) -> Self {
 		Self { conn, _root: root }
 	}
 
-    /// Runs the event loop - the very core of the window manager which receives events.
-    pub fn run(&self) -> xcb::Result<()> {
-        info!("Running the window manager");
-        loop {
-            match self.conn.wait_for_event()? {
-                xcb::Event::X(x::Event::ConfigureRequest(req)) => {
-                    trace!("Processing request to configure window ({})", req.window().resource_id());
-                }
-                xcb::Event::X(x::Event::MapRequest(req)) => {
-                    trace!("Processing request to map window ({})", req.window().resource_id());
-                }
-                xcb::Event::X(x::Event::ButtonPress(notif)) => {
-                    trace!("Processing button press ({})", notif.detail());
-                }
-                xcb::Event::X(x::Event::ButtonRelease(notif)) => {
-                    trace!("Processing button release ({})", notif.detail());
-                }
-                xcb::Event::X(x::Event::MotionNotify(_)) => {
-                    trace!("Processing cursor drag");
-                }
-                xcb::Event::X(x::Event::EnterNotify(notif)) => {
-                    trace!("Processing cursor-entered-window event ({})", notif.event().resource_id());
-                }
-                xcb::Event::X(x::Event::FocusIn(notif)) => {
-                    trace!("Processing newly focused window ({})", notif.event().resource_id());
-                }
-                _ => {}
-            }
-        }
-    }
+	/// Runs the event loop - the very core of the window manager which receives events.
+	pub fn run(&self) -> xcb::Result<()> {
+		info!("Running the window manager");
+		loop {
+			match self.conn.wait_for_event()? {
+				xcb::Event::X(x::Event::ConfigureRequest(req)) => {
+					trace!(
+						"Processing request to configure window ({})",
+						req.window().resource_id()
+					);
+				}
+				xcb::Event::X(x::Event::MapRequest(req)) => {
+					trace!(
+						"Processing request to map window ({})",
+						req.window().resource_id()
+					);
+				}
+				xcb::Event::X(x::Event::ButtonPress(notif)) => {
+					trace!("Processing button press ({})", notif.detail());
+				}
+				xcb::Event::X(x::Event::ButtonRelease(notif)) => {
+					trace!("Processing button release ({})", notif.detail());
+				}
+				xcb::Event::X(x::Event::MotionNotify(_)) => {
+					trace!("Processing cursor drag");
+				}
+				xcb::Event::X(x::Event::EnterNotify(notif)) => {
+					trace!(
+						"Processing cursor-entered-window event ({})",
+						notif.event().resource_id()
+					);
+				}
+				xcb::Event::X(x::Event::FocusIn(notif)) => {
+					trace!(
+						"Processing newly focused window ({})",
+						notif.event().resource_id()
+					);
+				}
+				_ => {}
+			}
+		}
+	}
 }
